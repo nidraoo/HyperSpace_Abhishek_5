63237af4bd29d089076ce2b223d29eb1c3a21d3d
Abhishek-2004-17 <117198301+Abhishek-2004-17@users.noreply.github.com>
2025-01-28 17:19:39 +0530

    Update warehouse SideBar.tsx

--- Frontend/app/warehouse/page.tsx
+++ Frontend/app/warehouse/page.tsx
@@ -1,10 +1,21 @@
-import { Timeline } from '@/components/warehouse/Timeline'
-import React from 'react'
 
-export default function page() {
-  return (
-<>
-<Timeline></Timeline>
-</>
-  )
-}
+import Cards from "@/components/client/Dashboard/Cards";
+import Chart from "@/components/client/Dashboard/Chart";
+import { SideBar } from "@/components/warehouse/SideBar";
+import React from "react";
+
+const Page = () => {
+  return <>
+    <SideBar />
+
+    <div className="sm:ml-72 mx-16 my-12">
+        <Cards />
+
+        <div className="my-20 flex justify-between gap-5 mx-20">
+              <Chart />
+        </div>
+    </div>
+  </>;
+};
+
+export default Page;
